922d548662fd55f5fcec081b81e6cfe1ed01de79
jesusvicente07 <jesusvicente007@gmail.com>
2020-01-10 16:47:05 -0700

    users

--- resources/views/adduser.blade.php
+++ resources/views/adduser.blade.php
@@ -1,45 +1,36 @@
 @extends('layouts.layout')
 @section('section')
+@section('title')
+Agregar nuevo usuario
+@endsection
 
-<form action="{{route('store')}}" method="POST">
+<div class="col-md-8">
+  <form action="{{route('store')}}" method="POST">
     @csrf
 
     <h3>Nombre:</h3>
-    <input type="text" name="name" class="form-control mb-2" value="{{ old('name') }}" autocomplete="off">
-    @error('name')
-    <div class="alert alert-danger">
-      El nombre es obligatorio
-    </div>
-    @enderror
+    <input type="text" name="name" class="form-control mb-2" value="{{ old('name') }}" autocomplete="off" required>
 
     <h3>Correo:</h3>
-    <input type="email" name="email" class="form-control mb-2" value="{{ old('email') }}" autocomplete="off">
-    @error('email')
-    <div class="alert alert-danger">
-      El correo es obligatorio
-    </div>
-    @enderror
-
+    <input type="email" name="email" class="form-control mb-2" value="{{ old('email') }}" autocomplete="off" required>
+ 
     <h3>Contraseña:</h3>
-    <input type="password" name="password" class="form-control mb-2" autocomplete="off">
-    @error('password')
-    <div class="alert alert-danger">
-    @if($errors->first('password')=='The password confirmation does not match.')
-    La contraseña no coincide
-    @else
-    La contraseña es obligatoria 
-    @endif 
-    </div>
-    @enderror
+    <input type="password" name="password" class="form-control mb-2" autocomplete="off" required>
     
     <h3> Confirmar contraseña:</h3>
     <input type="password" id="password-confirm" name="password_confirmation" class="form-control mb-2" autocomplete="off" required>
-    @error('password_confirmation')
+    @error('password')
+    @if($errors->first('password')=='The password confirmation does not match.')
     <div class="alert alert-danger">
-    La contraseña es obligatoria 
+    La contraseña no coincide
     </div>
+    @endif 
+    @elseif('password' == 'password_confirmation')
     @enderror
-    <button class="btn btn-primary btn-block" type="submit">Agregar</button>
-    </form>
+    <div class="float-right">
+    <button class="btn btn-primary" type="submit">Agregar nuevo usuario</button>
+    </div>
+  </form>
+</div>
 
 @endsection
