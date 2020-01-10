af545e256f2f00b816857e66bdd6646a408f989e
jan7alberto <jan7alberto@gmail.com>
2020-01-09 18:40:38 -0700

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,16 @@
 @extends('layouts.layout')
+@section('section')
+<div class="col-md-3">
+<div class="small-box bg-warning">
+    <div class="inner">
+    <h3>44</h3>
+
+    <p>Usuarios registrados</p>
+    </div>
+    <div class="icon">
+    <i class="ion ion-person-add"></i>
+    </div>
+    <a href="{{route('users')}}" class="small-box-footer">Mas Info<i class="fas fa-arrow-circle-right"></i></a>
+</div>
+</div>
+@endsection
